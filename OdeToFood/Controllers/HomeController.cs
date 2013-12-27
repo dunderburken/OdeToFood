41287f5e71c95c21a07266a260fc4a4d514d7608
mkryuk <mkryuk@gmail.com>
2013-12-27 13:08:52 +0200

    Part 4 done

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -10,9 +10,21 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         OdeToFoodDb _db = new OdeToFoodDb();
-        public ActionResult Index()
+        public ActionResult Index(string searchTerm = null)
         {
-            var model = _db.Restaurants.ToList();
+            var model = _db.Restaurants
+                        .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                        .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                        .Take(10)
+                        .Select(r =>new RestaurantListViewModel
+                        {
+                            Id = r.Id,
+                            Name = r.Name,
+                            City = r.City,
+                            Country = r.Country,
+                            CountOfReviews = r.Reviews.Count()
+
+                        });
             return View(model);
         }
 
--- OdeToFood/Migrations/Configuration.cs
+++ OdeToFood/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace OdeToFood.Migrations
 {
+    using OdeToFood.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -9,24 +11,22 @@ namespace OdeToFood.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "OdeToFood.Models.OdeToFoodDb";
         }
 
         protected override void Seed(OdeToFood.Models.OdeToFoodDb context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+            context.Restaurants.AddOrUpdate(r => r.Name,
+                new Restaurant { Name = "Sabatino's", City = "Baltimore", Country = "USA" },
+                new Restaurant { Name = "Great Lake", City = "Chikago", Country = "USA" },
+                new Restaurant
+                {
+                    Name = "Smaka",
+                    City = "Gothenburg",
+                    Country = "Sweden",
+                    Reviews = new List<RestaurantReview> { new RestaurantReview { Rating = 9, Body = "Great Food!!" } }
+                });
+        }        
     }
 }
