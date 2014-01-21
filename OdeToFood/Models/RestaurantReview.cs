7403c8cb1a06bfc8372ff6d3c157e6c426f93167
mkryuk <mkryuk@gmail.com>
2014-01-21 20:52:55 +0200

    Update in step

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -9,15 +9,19 @@ namespace OdeToFood.Models
     public class RestaurantReview
     {
         public int Id { get; set; }        
+
         [Range(1,10)]
         [Required]
         public int Rating { get; set; }
+        
         [Required]
         [StringLength(1024)]
         public string Body { get; set; }
+        
         public int RestaurantId { get; set; }
         [Display(Name="User Name")]
         [DisplayFormat(NullDisplayText="anonumous")]
+        
         public string ReviewerName { get; set; }
 
     }
